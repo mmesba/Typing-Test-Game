d0b906db0b475765b6702a62a6f045b83b7c6488
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-12-27 16:49:59 +0600

    server file initiated and necessary pacage installed

---
+++ server.js
@@ -0,0 +1,19 @@
+/*
+ * Title: Typing Test Game.
+ * Description: Server file for typing game app
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 27/12/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
